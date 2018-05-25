d397b55a5a65d8fe8f4383be9b9fe31bd56bbfbe
Stainley Lebron <stainley_lebron@claro.com.do>
2018-05-25 09:24:18 -0400

    First Commit

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+};
+
+class SearchBar extends React.Component {
+
+    renderSoftByOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+
+        });
+    }
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        renderSoftByOptions();
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
